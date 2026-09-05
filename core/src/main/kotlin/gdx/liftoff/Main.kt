51f47a2910f8ebbeb7d5606084c43c85b496886b
PAHVN <accphu89788@gmail.com>
2026-09-05 18:37:26 +0700

    fix bug build apk

--- core/src/main/kotlin/gdx/liftoff/Main.kt
+++ core/src/main/kotlin/gdx/liftoff/Main.kt
@@ -1,5 +1,6 @@
 package gdx.liftoff
 
+import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.ApplicationAdapter
